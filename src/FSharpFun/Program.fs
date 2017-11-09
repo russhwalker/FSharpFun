126ff99fce6dac29cef14569870dc92ba20f6692
RussW <russ.h.walker@gmail.com>
2017-11-08 20:17:29 -0500

    Initial Sync

---
+++ src/FSharpFun/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+let PrintHellowWorld() =
+    printfn "Hello World"
+
+[<EntryPoint>]
+let main argv = 
+    PrintHellowWorld()
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
