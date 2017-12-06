6c8b6212a006e3f5d35503e100ee0c723eafdb58
RussW <russ.h.walker@gmail.com>
2017-12-05 21:35:29 -0500

    Adding option matching.

--- src/FSharpFun.Library/OptionStuff.fs
+++ src/FSharpFun.Library/OptionStuff.fs
@@ -10,8 +10,34 @@ let person1 = { Name = "SomePerson1"; City = Some "Columbia" }
 let person2 = { Name = "SomePerson2"; City = None }
 
 let printPerson person =
-    printfn "%s" person.Name
+    let cityValue =
+        match person.City with
+        | Some n -> sprintf "%s" n
+        | None -> ""
+    printfn "Name:%s City: %s" person.Name cityValue
+
+
+type LivingThing = 
+    | Human of int
+    | PetDog of int
+
+let h = Human 5
+let d = PetDog 5
+let livingThings = [|h; d|]
+
+let Age (lt : LivingThing) =
+    match lt with
+    |Human x -> x
+    |PetDog y -> y * 7
+
+let totalAges = livingThings |> Array.sumBy(fun s -> Age s)
+
+let printAge lt =
+    printfn "%i" (Age(lt))
 
 let Run =
     printPerson(person1)
     printPerson(person2)
+    printAge(h)
+    printAge(d)
+    printfn "%i" totalAges
